d1e27446b67e5f345b18e609e268f295617c6b2d
Ty-Waltz <ty.waltz@yahoo.com>
2024-11-19 16:30:26 -0500

    finished javascript basics assignment

---
+++ script.js
@@ -0,0 +1,35 @@
+// task 1 + task 2
+studentName = "Alice";
+studentAge = 15;
+studentGrade = 10;
+isPresent = true;
+//task 3
+console.log(studentName);
+console.log(studentAge);
+console.log(studentGrade);
+console.log(isPresent);
+console.log("Test")
+
+// Exploring JavaScript Operators
+
+//task 1 + task 2
+let num1 = 22
+let num2 = 10
+// task 3
+console.log("Sum:", 2 + 2);
+console.log("Difference:", 4 - 2);
+console.log("Product:", 4 * 4);
+console.log("Quotient:", 12 / 4);
+//task 4 
+console.log("Updated num1:", 22);
+console.log("Updated num2:", 41);
+//task 5
+console.log("Is num1 equal to num2?", num1 == num2);
+console.log("Is num1 greater than num2?",num1 > num2 );
+console.log("Is num1 not equal to num2?", num1 != num2 );
+//task 6
+let isPositive = num1 > 0 && num2 > 0; 
+let isEven = num1 % 2 === 0 || num2 % 2 === 0; 
+
+console.log("Are both numbers positive?", isPositive); 
+console.log("Is at least one number even?", isEven); 
